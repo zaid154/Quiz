4d2c762753406eb6efcdba1a502a757aef9fa67f
Mohd Zaid <mohd.zaid.620268@gmail.com>
2026-01-17 18:08:48 +0530

    3 commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let registerpage = document.getElementById("register");
-let login = document.querySelector(".login"); // ✅ FIX
+let registerbtn = document.getElementById("register");
+let loginLink = document.getElementById("login_link");
+let login = document.querySelector(".login");
+let register = document.querySelector(".register");
 
-registerpage.addEventListener("click", () => {
-    login.style.display = "none";
+registerbtn.addEventListener("click", () => {
+  register.classList.add("active");
+  register.classList.remove("hidden");
+  login.classList.add("hidden");
+  login.classList.remove("active");
 });
 
+loginLink.addEventListener("click", () => {
+  login.classList.add("active");
+  login.classList.remove("hidden");
+  register.classList.add("hidden");
+  register.classList.remove("active");
+});
