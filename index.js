292e98a3d4bba6796f799ed4f7e37e5a06719bde
Mohd Zaid <mohd.zaid.620268@gmail.com>
2026-01-18 20:35:49 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// GET ELEMENTS
 let registerbtn = document.getElementById("register");
 let loginLink = document.getElementById("login_link");
+
+let logs = document.querySelectorAll(".log");
 
 let login = document.querySelector(".login");
 let register = document.querySelector(".register");
@@ -7,31 +10,48 @@ let register = document.querySelector(".register");
 let signinBtn = document.querySelector(".singnin");
 let registerBtn = document.querySelector(".register_btn");
 
-// REGISTER inputs
+// REGISTER INPUTS
 let create_fullname = document.querySelector("#name");
 let create_dob = document.querySelector("#dob");
 let create_email = document.querySelector("#email");
 let create_password = document.querySelector("#password");
 
-// LOGIN inputs
+//  LOGIN INPUTS
 let enter_email = document.querySelector(".enter_name");
 let enter_password = document.querySelector(".enter_password");
 
-/* ===== LOGIN CHECK ===== */
+icon.addEventListener("click", () => {
+  alert("Temporary service disabled");
+});
+
 signinBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (
-    enter_email.value.trim() === "" ||
-    enter_password.value.trim() === ""
-  ) {
+  if (enter_email.value.trim() === "" || enter_password.value.trim() === "") {
     alert("Both fields are compulsory");
+    return;
+  }
+
+  let storedUser = localStorage.getItem("user");
+
+  if (storedUser === null) {
+    alert("No account found. Please register first.");
+    return;
+  }
+
+  let user = JSON.parse(storedUser);
+
+  if (
+    enter_email.value === user.email &&
+    enter_password.value === user.password
+  ) {
+    alert("Login Successful ");
+    window.location.href = "question.html";
   } else {
-    alert("Login working ✅");
+    alert("Invalid Email or Password ");
   }
 });
 
-/* ===== REGISTER CHECK ===== */
 registerBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -42,9 +62,35 @@ registerBtn.addEventListener("click", (e) => {
     create_dob.value.trim() === ""
   ) {
     alert("All fields are compulsory");
-  } else {
-    alert("Account Created ✅");
+    return;
   }
+
+  let today = new Date();
+  let dob = new Date(create_dob.value);
+
+  today.setHours(0, 0, 0, 0);
+
+  if (dob > today) {
+    alert("DOB cannot be a future date ");
+    return;
+  }
+
+  let userData = {
+    name: create_fullname.value,
+    dob: create_dob.value,
+    email: create_email.value,
+    password: create_password.value,
+  };
+
+  localStorage.setItem("user", JSON.stringify(userData));
+
+  alert("Account Created Successfully ");
+
+  /* ===== SWITCH TO LOGIN ===== */
+  login.classList.add("active");
+  login.classList.remove("hidden");
+  register.classList.add("hidden");
+  register.classList.remove("active");
 });
 
 /* ===== TOGGLE FORMS ===== */
