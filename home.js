1b0baa2250da792bdb5d907685f2c2ddb29b9bc7
Mohd Zaid <mohd.zaid.620268@gmail.com>
2026-01-21 19:15:27 +0530

    commit

--- home.js
+++ home.js
@@ -3,15 +3,15 @@ let home_email = document.getElementById("home_email");
 
 
 
-let html_btn = document.getElementById("q_html");
-let css_btn  = document.getElementById("q_css");
-let js_btn   = document.getElementById("q_js");
-let c_btn    = document.getElementById("q_c");
-let cpp_btn  = document.getElementById("q_cpp");
-let php_btn  = document.getElementById("q_php");
-let py_btn   = document.getElementById("q_py");
-let java_btn = document.getElementById("q_java");
-let sql_btn  = document.getElementById("q_sql");
+let html_btn = document.getElementById("btn_html");
+let css_btn  = document.getElementById("btn_css");
+let js_btn   = document.getElementById("btn_js");
+let c_btn    = document.getElementById("btn_c");
+let cpp_btn  = document.getElementById("btn_cpp");
+let php_btn  = document.getElementById("btn_php");
+let py_btn   = document.getElementById("btn_python");
+let java_btn = document.getElementById("btn_java");
+let sql_btn  = document.getElementById("btn_sql");
 
 
 
@@ -44,3 +44,25 @@ goToQuiz(java_btn, "./quiz_question/java_quiz.html");
 goToQuiz(sql_btn,  "./quiz_question/sql_quiz.html");
 
 
+let searchInput = document.getElementById("searchQuiz");
+let topics = document.querySelectorAll(".topic");
+
+searchInput.addEventListener("keyup", () => {
+  let value = searchInput.value.toLowerCase();
+
+  topics.forEach(topic => {
+    let topicId = topic.id.toLowerCase(); // q_html, q_css etc
+
+    if (topicId.includes(value)) {
+      topic.style.display = "flex";
+    } else {
+      topic.style.display = "none";
+    }
+  });
+});
+let logoutBtn = document.querySelector(".logout-btn");
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("loginUser");
+  window.location.href = "index.html";
+});
